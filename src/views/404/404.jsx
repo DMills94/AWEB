149db3d2fc1ccda1a4db844daff3eb01319af988
DMills94 <davidj-mills@live.co.uk>
2019-04-13 02:36:19 +0100

    update 404 page styling

--- src/views/404/404.jsx
+++ src/views/404/404.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+import './404.scss'
+
 export default function Page404() {
     return (
-        <div>
-            <h4>This page doesn't exist! <NavLink to='/'>Return to the homepage</NavLink></h4>
-        </div>
+        <section className='page404 flex column center'>
+            <h1>This page doesn't exist!</h1>
+            <NavLink to='/'>Return to the homepage</NavLink>
+        </section>
     )
 }
