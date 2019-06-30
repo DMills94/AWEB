32d57c5b184188b1772064d43e0be65339f9a415
DMills94 <davidj-mills@live.co.uk>
2019-06-30 16:20:31 +0100

    new Footer style

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 
-//Styles
+// Styles
 import './Footer.scss'
+
+// Assets
+import instagram from '../../assets/icons/instagram.svg'
+import facebook from '../../assets/icons/facebook.svg'
+import telegram from '../../assets/icons/telegram.svg'
+import linkedIn from '../../assets/icons/linkedin.svg'
 
 const Footer = () => {
     return (
         <div className='footer flex align-v'>
             <div className='contact-wrap flex column align-h'>
-                <p className='brown'>I'm social</p>
+                <p className='email'>hello@albinacholak.com</p>
                 <div className='social-media-wrap flex'>
-                    <a target='_blank' rel='noopener' href='https://www.linkedin.com/in/cholak/'>LinkedIn</a>
-                    <a target='_blank' rel='noopener' href='https://t.me/MindMeanDesign'>Telegram</a>
-                    <a target='_blank' rel='noopener' href='https://www.facebook.com/albina.cholak'>Facebook</a>
-                    <a target='_blank' rel='noopener' href='https://www.instagram.com/albina.cholak/'>Instagram</a>
+                    <a className='icon' target='_blank' rel='noopener' href='https://www.linkedin.com/in/cholak/'><img src={linkedIn} alt="LinkedIn"/></a>
+                    <a className='icon' target='_blank' rel='noopener' href='https://t.me/MindMeanDesign'><img src={telegram} alt="Telegram"/></a>
+                    <a className='icon' target='_blank' rel='noopener' href='https://www.facebook.com/albina.cholak'><img src={facebook} alt="Facebook"/></a>
+                    <a className='icon' target='_blank' rel='noopener' href='https://www.instagram.com/albina.cholak/'><img src={instagram} alt="Instagram"/></a>
                 </div>
             </div>
-            <a className='email' href="mailto:hello@albinacholak.com">hello@albinacholak.com</a>
         </div>
     )
 }
