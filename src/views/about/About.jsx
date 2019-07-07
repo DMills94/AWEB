dfe866457ed7611ccfda5b1bad3bbd54d0b92b02
DMills94 <davidj-mills@live.co.uk>
2019-07-07 13:32:46 +0100

    [TEMP] Remove insta api as it's shite

--- src/views/about/About.jsx
+++ src/views/about/About.jsx
@@ -1,5 +1,4 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React from 'react'
 
 // Assets
 import CV from '../../assets/icons/cv.svg'
@@ -10,7 +9,10 @@ import overflow from '../../assets/icons/overflow.svg'
 import pin from '../../assets/icons/pin.svg'
 import sketch from '../../assets/icons/sketch.svg'
 import valuesOne from '../../assets/icons/value-number-1.svg'
-
+import insta1 from '../../assets/images/insta1.jpg'
+import insta2 from '../../assets/images/insta2.jpg'
+import insta3 from '../../assets/images/insta3.jpg'
+import insta4 from '../../assets/images/insta4.jpg'
 // Styles
 import './About.scss'
 
@@ -64,116 +66,114 @@ const growthItems = [
         text: 'Solve business problems and translate it to a beautiful digital solution that customers love.',
         link: 'Hire me to give a talk'
     },
-    
 ]
 
-class About extends Component {
-    state = {
-        insta: null
+const instaItems = [
+    {
+        url: 'https://www.instagram.com/p/BxO_fW3AbvJ/',
+        imageUrl: insta1
+    },
+    {
+        url: 'https://www.instagram.com/p/BxIF68bAA-g/',
+        imageUrl: insta2
+    },
+    {
+        url: 'https://www.instagram.com/p/Bw-F_4mAvtH/',
+        imageUrl: insta3
+    },
+    {
+        url: 'https://www.instagram.com/p/BwFEgMuAwKr/',
+        imageUrl: insta4
     }
+]
 
-    componentDidMount = async () => {
-        const insta = (await axios.get('https://www.instagram.com/mindmeandsgn/?__a=1')).data.graphql.user.edge_owner_to_timeline_media.edges
-        
-        this.setState({
-            insta: insta.slice(0, 4)
-        })
-    }
-
-    renderInstagram = () => {
-        return this.state.insta.map(post => (
-            <img src={post.node.display_url} key={post.node.id} alt='Instagram1'/>
-        ))
-    }
-
-    render() {
-        return (
-            <section className='about'>
-                <div className='container'>
-                    <div className='intro flex align-v'>
-                        <div className='right'>
-                            <h2>
-                                Hello 😉
-                                <br />
-                                <span className='bold'>
-                                    I'm Albina Cholak,
-                                    <br />a Lead Product Designer.
-                                </span>
-                            </h2>
-                            <p>
-                                I’m driven by business outcomes, in love
-                                <br />
-                                with technical possibilities and empowered
-                                <br />
-                                with design tools & methods.
-                            </p>
-                            <p>Currently at Octopus Labs.</p>
-                            <button className='btn resume dis-fc'>
-                                <img src={CV} alt='' />
-                                Request resume
-                            </button>
-                        </div>
-                        <div className='left'>
-                            <img src={aboutPortrait} alt='' />
-                        </div>
+const About = () => {
+    return (
+        <section className='about'>
+            <div className='container'>
+                <div className='intro flex align-v'>
+                    <div className='right'>
+                        <h2>
+                            Hello 😉
+                            <br />
+                            <span className='bold'>
+                                I'm Albina Cholak,
+                                <br />a Lead Product Designer.
+                            </span>
+                        </h2>
+                        <p>
+                            I’m driven by business outcomes, in love
+                            <br />
+                            with technical possibilities and empowered
+                            <br />
+                            with design tools & methods.
+                        </p>
+                        <p>Currently at Octopus Labs.</p>
+                        <button className='btn resume dis-fc'>
+                            <img src={CV} alt='' />
+                            Request resume
+                        </button>
                     </div>
-
-                    <div className='values'>
-                        <h2 className='title'>My Values</h2>
-                        <div className='items-wrap flex wrap'>
-                            {valuesItems.map((item, i) => {
-                                return (
-                                    <div className='item' key={i}>
-                                        <img src={valuesOne} alt=''/>
-                                        <p className='sub-title'>{item.title}</p>
-                                        <p className='small'>{item.text}</p>
-                                    </div>
-                                )
-                            })}
-                        </div>
+                    <div className='left'>
+                        <img src={aboutPortrait} alt='' />
                     </div>
-
-                    <div className='growth'>
-                        <h2 className='title'>How I help grow businesses</h2>
-                        <div className='items-wrap flex wrap'>
-                            {growthItems.map((item, i) => {
-                                return (
-                                    <div className='item flex column' key={i}>
-                                        <h3 className='sub-title'>{item.title}</h3>
-                                        <p className='text'>{item.text}</p>
-                                        <div className='link flex align-v'>
-                                            <img src={pin} alt=''/>
-                                            <p>{item.link}</p>
-                                        </div>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    </div>
-
-                    <div className='design flex column centre'>
-                        <h3>I'm powered by the best design tools</h3>
-                        <div className='items-wrap flex'>
-                            <img src={abstract} alt='Abstract'/>
-                            <img src={sketch} alt='Sketch'/>
-                            <img src={overflow} alt='Overflow'/>
-                            <img src={miro} alt='Miro'/>
-                        </div>
-                    </div>
-
-                    {this.state.insta && 
-                        <div className='instagram'>
-                            <h2>My Instagram</h2>
-                            <a href='https://instagram.com/mindmeandsgn' target='_blank' rel='noopener noreferrer'>@MindMeanDesign</a>
-                            <div className='items-wrap flex'>
-                                {this.renderInstagram()}
-                            </div>
-                        </div>
-                    }
                 </div>
-            </section>
-        )
-    }
+
+                <div className='values'>
+                    <h2 className='title'>My Values</h2>
+                    <div className='items-wrap flex wrap'>
+                        {valuesItems.map((item, i) => {
+                            return (
+                                <div className='item' key={i}>
+                                    <img src={valuesOne} alt=''/>
+                                    <p className='sub-title'>{item.title}</p>
+                                    <p className='small'>{item.text}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+
+                <div className='growth'>
+                    <h2 className='title'>How I help grow businesses</h2>
+                    <div className='items-wrap flex wrap'>
+                        {growthItems.map((item, i) => {
+                            return (
+                                <div className='item flex column' key={i}>
+                                    <h3 className='sub-title'>{item.title}</h3>
+                                    <p className='text'>{item.text}</p>
+                                    <div className='link flex align-v'>
+                                        <img src={pin} alt=''/>
+                                        <p>{item.link}</p>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+
+                <div className='design flex column centre'>
+                    <h3>I'm powered by the best design tools</h3>
+                    <div className='items-wrap flex'>
+                        <img src={abstract} alt='Abstract'/>
+                        <img src={sketch} alt='Sketch'/>
+                        <img src={overflow} alt='Overflow'/>
+                        <img src={miro} alt='Miro'/>
+                    </div>
+                </div>
+
+                <div className='instagram'>
+                    <h2>My Instagram</h2>
+                    <a href='https://instagram.com/mindmeandsgn' target='_blank' rel='noopener noreferrer'>@MindMeanDesign</a>
+                    <div className='items-wrap flex'>
+                        {instaItems.map((image, i) => {
+                            return <a href={image.url} target='_blank' rel='noopener noreferrer'><img src={image.imageUrl} key={i} alt='Instagram1'/></a>
+                        })}
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default About
