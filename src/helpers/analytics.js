88c5cda1aee0bae0b595e8dcc753e633ca14f210
DMills94 <davidj-mills@live.co.uk>
2019-09-14 14:52:43 +0100

    false for always default GA

--- src/helpers/analytics.js
+++ src/helpers/analytics.js
@@ -1,6 +1,6 @@
 import ReactGA from 'react-ga'
 
-export const initGA = () => ReactGA.initialize([{ trackingId: 'UA-148031226-1' }, { trackingId: 'UA-148031226-2' }], { debug: true, alwaysSendToDefaultTracker: true })
+export const initGA = () => ReactGA.initialize([{ trackingId: 'UA-148031226-1' }, { trackingId: 'UA-148031226-2' }], { debug: true, alwaysSendToDefaultTracker: false })
 
 export const logPageView = path => {
     ReactGA.set({ page: path })
