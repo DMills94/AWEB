b0e5ec0b2f2c5deff5236cadeb61a991d32c6b16
DMills94 <davidj-mills@live.co.uk>
2019-09-14 12:00:12 +0100

    Add netlify form stuff, remove all Class components in favour of hooks

--- src/components/base/text-input/TextInput.jsx
+++ src/components/base/text-input/TextInput.jsx
@@ -1,60 +1,67 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 //Styles
 import '../Inputs.scss'
 
-class TextInput extends Component {
-    state = {
-        focused: false
+const TextInput = props => {
+    const [focused, setFocused] = useState(false)
+
+    const onFocus = () => {
+        setFocused(true)
     }
 
-    onFocus = () => {
-        this.setState({
-            focused: true
-        })
+    const onBlur = () => {
+        setFocused(false)
     }
 
-    onBlur = () => {
-        this.setState({
-            focused: false
-        })
+    const clickInput = () => {
+        setFocused(true)
     }
 
-    clickInput = () => {
-        this.textInput.focus()
-    }
+    const labelContent = props.value === ''
+        ? ''
+        : ' content'
 
-    render() {
-        const labelContent = this.props.value === ''
-            ? ''
-            : ' content'
+    const isFocused = focused
+        ? ' focus'
+        : ''
 
-        const focused = this.state.focused
-            ? ' focus'
-            : ''
+    return (
+        <div className='input-wrap'>
+            <label
+                className={'label' + labelContent + isFocused}
+                onClick={() => clickInput()}
+            >
+                {props.label}
+            </label>
+            <input
+                onChange={e => props.handleChange(e)}
+                onFocus={() => onFocus()}
+                onBlur={() => onBlur()}
+                type={props.type}
+                name={props.name}
+                value={props.value}
+                className={isFocused}
+            />
+        </div>
+    )
+}
 
-        return (
-            <div className='input-wrap'>
-                <label
-                    className={'label' + labelContent + focused}
-                    onClick={() => this.clickInput()}
-                >
-                    {this.props.label}
-                </label>
-                <input
-                    ref={input => {this.textInput = input}}
-                    onChange={e => this.props.handleChange(e)}
-                    onFocus={() => this.onFocus()}
-                    onBlur={() => this.onBlur()}
-                    type='text'
-                    name={this.props.name}
-                    value={this.props.value}
-                    className={focused}
-                />
-            </div>
-        )
-    }
+TextInput.propTypes = {
+    handleChange: PropTypes.func.isRequired,
+    label: PropTypes.string,
+    name: PropTypes.string,
+    type: PropTypes.string,
+    value: PropTypes.string.isRequired
+}
+
+TextInput.defaultProps = {
+    handleChange: () => {},
+    label: '',
+    name: '',
+    type: 'text',
+    value: ''
 }
 
 export default TextInput
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
 // Components
 import TextInput from '../base/text-input/TextInput'
@@ -9,73 +9,68 @@ import './Contact.scss'
 
 // Assets
 import close from '../../assets/icons/simple-close.svg'
-import smallLogo from '../../assets/icons/small-logo.svg'
 
-class Contact extends Component {
-	state = {
+const Contact = props => {
+	const [form, setForm] = useState({
 		name: '',
 		email: '',
 		message: ''
-	}
+	})
 
-	handleInputChange = event => {
-		const value = event.target.value
-		const name = event.target.name
+	const handleInputChange = event => {
+		const { name, value } = event.target
 
-		this.setState({
+		setForm({
+			...form,
 			[name]: value
 		})
 	}
 
-	submitForm = () => {
-		//Submit the form.... somehow
-	}
+	const display = props.show
+		? ''
+		: ' dis-n'
 
-	render() {
-		const display = this.props.show
-			? ''
-			: ' dis-n'
-
-		return (
-			<div className={'contact-background' + display} onClick={this.props.toggleModal}>
-				<div className={'contact-modal flex centre' + display}>
-					<img src={close} onClick={this.props.toggleModal} className='close' alt='X'/>
-					<section className='contact-body'>
-						<h2>Say hello! 👋</h2>
-						<p>Email <a href='mailto:albina.cholak@gmail.com'>albina.cholak@gmail.com</a><br />
-						Or fill out this form:</p>
-						<form
-							onSubmit={() => this.submitForm()}
-						>
-							<TextInput
-								label='Your name'
-								value={this.state.name}
-								name='name'
-								handleChange={event => this.handleInputChange(event)}
-							/>
-							<TextInput
-								label='Your email'
-								value={this.state.email}
-								name='email'
-								handleChange={event => this.handleInputChange(event)}
-							/>
-							<TextArea
-								label='Your message'
-								value={this.state.message}
-								name='message'
-								handleChange={event => this.handleInputChange(event)}
-							/>
-							<button
-								type='submit'
-							>
-								Send
-							</button>
-						</form>
-					</section>
-				</div>
+	return (
+		<div className={'contact-background' + display} onClick={props.toggleModal}>
+			<div className={'contact-modal flex centre' + display}>
+				<img src={close} onClick={props.toggleModal} className='close' alt='X'/>
+				<section className='contact-body'>
+					<h2>Say hello! 👋</h2>
+					<p>Email <a href='mailto:hello@albinacholak.com'>hello@albinacholak.com</a><br />
+					Or fill out this form:</p>
+					<form
+						name='contact'
+						method='POST'
+					>
+						<input type='hidden' name='form-name' value='contact' />
+						<TextInput
+							label='Your name'
+							value={form.name}
+							name='name'
+							type='text'
+							handleChange={event => handleInputChange(event)}
+						/>
+						<TextInput
+							label='Your email'
+							value={form.email}
+							name='email'
+							type='email'
+							handleChange={event => handleInputChange(event)}
+						/>
+						<TextArea
+							label='Your message'
+							value={form.message}
+							name='message'
+							handleChange={event => handleInputChange(event)}
+						/>
+						<button type='submit'>
+							Send
+						</button>
+					</form>
+				</section>
 			</div>
-		)
-	}
+		</div>
+	)
 }
 
 export default Contact
